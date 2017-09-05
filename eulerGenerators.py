6d30c6a2eecb3a84f97b8b1d1e35ba2be73568e7
jfontsaballs <jfontsaballs@gmail.com>
2017-09-05 21:01:54 +0200

    Triangle numbers generation.

--- eulerGenerators.py
+++ eulerGenerators.py
@@ -7,18 +7,28 @@ from eulerExceptions import ArgumentMustBeNonNegativeInteger
 from timeit import timeit
 import numbers
 
-def fibonacci(max):
+def fibonacci(maximum):
     """
     >>> list(fibonacci(60))
     [1, 2, 3, 5, 8, 13, 21, 34, 55]
     """
     a = 1
     b = 1
-    while b < max:
+    while b < maximum:
         yield b
         a, b = b, a + b
 
-def primes(max):
+def triangleNumbers(maximum):
+    """
+    >>> list(triangleNumbers(10))
+    [1, 3, 6, 10, 15, 21, 28, 36, 45, 55]
+    """
+    sum = 0
+    for i in range(1, maximum + 1):
+        sum += i
+        yield sum
+
+def primes(maximum):
     """
     >>> list(primes(1000.0))
     [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
@@ -31,16 +41,16 @@ def primes(max):
     yield 3
     yield 5
     yield 7
-    for candidate in range(12, int(max) + 10, 6):
+    for candidate in range(12, int(maximum) + 10, 6):
         nextCandidate = candidate - 1
-        if nextCandidate > max:
+        if nextCandidate > maximum:
             break
         if _testCandidate(nextCandidate, primes):
             primes.append(nextCandidate)
             yield nextCandidate
 
         nextCandidate = candidate + 1
-        if nextCandidate > max:
+        if nextCandidate > maximum:
             break
         if _testCandidate(nextCandidate, primes):
             primes.append(nextCandidate)
